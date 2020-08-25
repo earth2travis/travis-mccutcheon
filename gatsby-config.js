99ae63ba79e6b167db00590ecda3afba7d175341
earth2travis <travis.mccutcheon@gmail.com>
2020-08-25 18:22:08 -0500

    Update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Travis McCutcheon`,
+    description: `Personal web page of Travis McCutcheon`,
+    author: `@earth2travis`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
